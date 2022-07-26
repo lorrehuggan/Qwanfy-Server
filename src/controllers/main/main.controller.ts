e5b82a26467e651059520a19a0d8786719dcaf72
lorre huggan <lorre.huggan@protonmail.com>
2022-07-26 13:35:41 +0100

    put main in try catch

--- src/controllers/main/main.controller.ts
+++ src/controllers/main/main.controller.ts
@@ -44,54 +44,62 @@ export const mainController = async (
 
   //-------->Get recommended artist top tracks<--------
   if (recommendedArtists.length > 0) {
-    const artistIDs = recommendedArtists.map((artist) => artist.id);
-    const response = await Promise.all(
-      artistIDs.map((id) => spotifyApi.getArtistTopTracks(id, 'US'))
-    );
-    const data = response.map((data) => {
-      return data.body.tracks;
-    });
-
-    data.forEach((item) => {
-      topTracks.push(
-        {
-          id: item[1].id,
-          name: item[1].name,
-          artist: item[1].artists,
-          album: item[1].album.name,
-          preview_url: item[1].preview_url,
-          images: item[1].album.images,
-          albumURL: item[1].album.external_urls.spotify,
-          popularity: item[1].popularity,
-          explicit: item[1].explicit,
-          duration: item[1].duration_ms,
-        },
-        {
-          id: item[2].id,
-          name: item[2].name,
-          artist: item[2].artists,
-          album: item[2].album.name,
-          preview_url: item[2].preview_url,
-          images: item[2].album.images,
-          albumURL: item[2].album.external_urls.spotify,
-          popularity: item[2].popularity,
-          explicit: item[2].explicit,
-          duration: item[2].duration_ms,
-        },
-        {
-          id: item[3].id,
-          name: item[3].name,
-          artist: item[3].artists,
-          album: item[3].album.name,
-          preview_url: item[3].preview_url,
-          images: item[3].album.images,
-          albumURL: item[3].album.external_urls.spotify,
-          popularity: item[3].popularity,
-          explicit: item[3].explicit,
-          duration: item[3].duration_ms,
-        }
+    try {
+      const artistIDs = recommendedArtists.map((artist) => artist.id);
+      const response = await Promise.all(
+        artistIDs.map((id) => spotifyApi.getArtistTopTracks(id, 'US'))
       );
-    });
+      const data = response.map((data) => {
+        return data.body.tracks;
+      });
+
+      if (data) {
+        data.forEach((item) => {
+          topTracks.push(
+            {
+              id: item[1].id,
+              name: item[1].name,
+              artist: item[1].artists,
+              album: item[1].album.name,
+              preview_url: item[1].preview_url,
+              images: item[1].album.images,
+              albumURL: item[1].album.external_urls.spotify,
+              popularity: item[1].popularity,
+              explicit: item[1].explicit,
+              duration: item[1].duration_ms,
+            },
+            {
+              id: item[2].id,
+              name: item[2].name,
+              artist: item[2].artists,
+              album: item[2].album.name,
+              preview_url: item[2].preview_url,
+              images: item[2].album.images,
+              albumURL: item[2].album.external_urls.spotify,
+              popularity: item[2].popularity,
+              explicit: item[2].explicit,
+              duration: item[2].duration_ms,
+            },
+            {
+              id: item[3].id,
+              name: item[3].name,
+              artist: item[3].artists,
+              album: item[3].album.name,
+              preview_url: item[3].preview_url,
+              images: item[3].album.images,
+              albumURL: item[3].album.external_urls.spotify,
+              popularity: item[3].popularity,
+              explicit: item[3].explicit,
+              duration: item[3].duration_ms,
+            }
+          );
+        });
+      } else {
+        next(ApiError.badRequest('Bad request something went wrong'));
+      }
+    } catch (error: any) {
+      next(ApiError.badRequest('Bad request something went wrong'));
+    }
   } else {
     next(ApiError.badRequest('Bad request something went wrong'));
   }
